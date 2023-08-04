d577d2379a8b61e589d20f8e0b8341a22ce097c7
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2023-08-04 11:03:23 -0300

    correcao1

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,8 +1,8 @@
 <?php
 
-defined ('BASEPATH') OR exit('Acao nao permitida');
+defined('BASEPATH') OR exit('Acao nao permitida');
 
-class Usuarios extends CI_Controller {
+class Usuarios extends CI_Controller{
 
     public function __construct() {
         parent::__construct();
