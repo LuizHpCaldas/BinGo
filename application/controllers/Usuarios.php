ceb8b15bc33ca891d5f47ed9a4e6815c33f15420
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2023-08-04 10:34:59 -0300

    Initial commit

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,17 @@
+<?php
+
+defined ('BASEPATH') OR exit('Acao nao permitida');
+
+class Usuarios extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+         //Definir se existe sessao
+
+    }
+
+    public function index() {
+        
+    }
+   
+}
