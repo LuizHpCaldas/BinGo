1e3d6fdddddfb26aa36a4438fbf0baf1a6f0fade
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2023-08-13 11:37:10 -0300

    autenticacao

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,6 +4,21 @@ defined('BASEPATH') OR exit ('Acao nao permitida');
 
 class Login extends CI_Controller{
     public function index(){
-        $this->load->view('login/index');
+
+
+        $identity = $this->security->xss_clean($this->input->post('email'));
+        $password = $this->security->xss_clean($this->input->post('password'));
+        $remember = FALSE; // remember the user
+        
+        if($this->ion_auth->login($identity, $password)){
+            redirect('home');
+        }else{
+            echo 'Erro de validacao';
+            $this->load->view('layout/header');
+            $this->load->view('login/index');
+        }
+
+
+
     }
 }
---
+++ application/views/login/index.php
@@ -0,0 +1,47 @@
+
+
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-8 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5" style="margin-top: 15rem !important">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-5 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Bem vindo(a) de volta!</h1>
+                  </div>
+                  <form class="user" name="form_index" method="POST">
+                    <div class="form-group">
+                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Entre com seu email...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Senha">
+                    </div>
+
+                    <button type="submit"  class="btn btn-primary btn-user btn-block">
+                      Entrar <i class="fas fa-sign-in-alt"></i>
+</button>
+                  
+                  </form>
+               
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+ 
